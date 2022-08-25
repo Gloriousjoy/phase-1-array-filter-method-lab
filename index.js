ec7b6bea028d3921e382f0ea605b02f66f6c2282
Gloriousjoy <gloriousfab99@gmail.com>
2022-08-25 18:40:39 +0300

    array method-filter

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = []
+
+function findMatching(drivers, string){
+    return drivers.filter (
+        (matchingName) => matchingName.toLowerCase() === string.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter (
+        (matchingName) => matchingName.toLowerCase().indexOf(string.toLowerCase()) === 0
+    );
+}
+
+function matchName(drivers, string){
+    return drivers.filter((data => data.name === string));
+
+}
